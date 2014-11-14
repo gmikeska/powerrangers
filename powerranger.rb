d02dce191a20e376e239fc323ac735a1cf0a040a
greg <gmikeska07@gmail.com>
2014-11-14 10:23:23 -0600

    Fights module to reduce redundancy

--- powerranger.rb
+++ powerranger.rb
@@ -1,5 +1,18 @@
-#module 
+module Fights
+
+	def punch (target) 
+		puts "Punching #{target.name} with strength #{@strength}! (From Module)"
+		target.scream()
+		target.run()
+		target.caffeine_level-=1
+		@caffeine_level-=1
+	end
+
+
+end
+
 class Person
+	include Fights
 	attr_reader :name
 	attr_accessor :caffeine_level
 	@name
@@ -10,16 +23,17 @@ class Person
 		puts "#{@name} appears!"
 
 	end
-		
 
 	def run
 		puts "#{@name} runs away!" 
 
 	end
+
 	def scream 
 		puts "#{@name} says AHHHH!"
 
 	end
+
 	def drink_coffee 
 		@caffeine_level+=1
 		puts "#{@name} is drinking coffee. Caffeine Level is now #{@caffeine_level}."
@@ -39,13 +53,7 @@ class PowerRanger < Person
 		puts "#{@name} appears!"
 
 	end
-	def punch (target) 
-		puts "Punching #{target.name} with strength #{@strength}!"
-		target.scream()
-		target.run()
-		target.caffeine_level-=1
-		@caffeine_level-=1
-	end
+
 
 	def rest 
 		puts "#{@name} is resting."
@@ -72,14 +80,6 @@ class EvilNinja < Person
 		@@ninjas+=1
 		@name = "Evil Ninja #{@@ninjas}"
 		puts "#{@name} appears!"
-	end
-
-	def punch (target) 
-		puts "#{@name} punches #{target.name} with strength #{@strength}!"
-		target.scream()
-		target.run()
-		target
-		@caffeine_level-=1
 	end
 
 	def cause_mayhem(target)
